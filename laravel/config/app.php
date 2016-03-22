c8ec5dbb490d4b00096c47d06375af9a16ac4e32
Wesley Machado <weslley182@gmail.com>
2016-03-21 21:58:19 -0300

    adicionando scafold

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -147,6 +147,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Bestmomo\Scafold\ScafoldServiceProvider::class,
+
 
         /*
          * Application Service Providers...
