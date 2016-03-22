60559d0911ed9624da1af27bda4210cfb3104214
Wesley Machado <weslley182@gmail.com>
2016-03-21 23:46:39 -0300

    detalhes por route

--- laravel/app/Http/Controllers/ProdutoController.php
+++ laravel/app/Http/Controllers/ProdutoController.php
@@ -3,22 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\Request;
 
 class ProdutoController extends Controller{
     public function lista(){
-        $html = '<h1>Listagem de produtos com Laravel</h1>';
-
-        $html .= '<ul>';
-
         $produtos = DB::select("select * from produtos");
 
-        foreach ($produtos as $p) {
-            $html .= '<li> Nome: '. $p->nome .', Descrição: '. $p->descricao .'</li>';
+        return view('listagem')->with('produtos', $produtos);
+    }
+
+    public function mostra(){
+        $id = Request::route('id');
+        $resposta = DB::select('select * from produtos where id = ?', [$id]);
+
+        if(empty($resposta)) {
+            return "Esse produto não existe";
         }
-
-        $html .= '</ul>';
-
-        return $html;
+        return view('detalhes')->with('p', $resposta[0]);
     }
 }
---
+++ laravel/resources/views/detalhes.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="/css/app.css" rel="stylesheet">
+    <title>listagem</title>
+</head>
+<body>
+<div class="container">
+    <h1>Detalhes do produto: <?= $p->nome ?> </h1>
+
+    <ul>
+        <li>
+            <b>Valor:</b> R$ <?= $p->valor ?>
+        </li>
+        <li>
+            <b>Descrição:</b> <?= $p->descricao ?>
+        </li>
+        <li>
+            <b>Quantidade em estoque:</b> <?= $p->quantidade ?>
+        </li>
+    </ul>
+</div>
+</body>
+</html>
